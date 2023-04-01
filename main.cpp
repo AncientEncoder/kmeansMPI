276a14ca6d2dce684df520e07c37e280d4daa865
Xieyu Wang <varianovsky@outlook.com>
2023-04-01 18:24:34 +0300

    .

--- main.cpp
+++ main.cpp
@@ -13,9 +13,23 @@ int main(int argc,char*argv[]){
     }
     DIR_OBJECT=argv[1];
     IOController::IOController ioController;
-    KMeans::KMeans kMeans(std::stod(argv[2]),std::atoi(argv[3]),std::atoi(argv[4]),argc,argv);
-    kMeans.setData(ioController.fileReader(DIR_OBJECT));
-    kMeans.KMeansRun();
-    ioController.fileWriter(DIR_WRITE,kMeans.getClusterData());
+    MPI_Init(&argc, &argv);
+    int num_procs, rank;
+    MPI_Comm_size(MPI_COMM_WORLD, &num_procs);
+    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+
+    // only rank 0 creates KMeans object
+    KMeans::KMeans* kmeans;
+    if (rank == 0) {
+        kmeans = new KMeans::KMeans(std::stod(argv[2]),std::atoi(argv[3]),std::atoi(argv[4]),argc,argv);
+        kmeans->setData(ioController.fileReader(DIR_OBJECT));
+        kmeans->KMeansRun();
+        ioController.fileWriter(DIR_WRITE,kmeans->getClusterData());
+        delete kmeans;
+
+        MPI_Finalize();
+    }
+    //KMeans::KMeans kMeans(std::stod(argv[2]),std::atoi(argv[3]),std::atoi(argv[4]),argc,argv);
+
     return 0;
 }
